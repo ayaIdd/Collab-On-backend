a27fcd934d5b3bf76494e8104dec9541c18233c5
sara_bentafat <s_bentafat@estin.dz>
2024-10-09 20:49:51 +0100

    change cors origin from :3000 to http://localhost:5173

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
-    methods: ["GET", "POST", "DELETE", "PUT" , "PATCH"],
+    origin: ["http://localhost:5173"],
+    methods: ["GET", "POST", "DELETE", "PUT", "PATCH"],
     credentials: true,
   })
 );
